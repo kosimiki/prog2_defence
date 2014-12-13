61e424e873d8caf826548f8840ba53ca4fa461e4
Kosharkar <kosimiki@gmail.com>
2014-12-13 15:36:19 +0100

    megjelenito es egy par finomitas

---
+++ Megjelenito/src/main/java/sample/Sample3.java
@@ -0,0 +1,162 @@
+import java.util.*;
+import java.awt.BorderLayout;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.event.MouseInputListener;
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.input.CenterMapListener;
+import org.jxmapviewer.input.PanKeyListener;
+import org.jxmapviewer.input.PanMouseInputListener;
+import org.jxmapviewer.input.ZoomMouseWheelListenerCursor;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.LocalResponseCache;
+import org.jxmapviewer.viewer.TileFactoryInfo;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.DefaultWaypoint;
+import org.jxmapviewer.viewer.Waypoint;
+import org.jxmapviewer.viewer.WaypointPainter;
+import org.jxmapviewer.painter.CompoundPainter;
+import org.jxmapviewer.painter.Painter;
+
+/**
+ * A simple sample application that shows
+ * a OSM map of Europe
+ * @author Martin Steiger
+ */
+public class Sample3
+{
+	/**
+	 * @param args the program args (ignored)
+	 */
+	public static void main(String[] args)
+	{
+		// Create a TileFactoryInfo for OpenStreetMap
+		TileFactoryInfo info = new OSMTileFactoryInfo();
+		DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+		tileFactory.setThreadPoolSize(8);
+
+		// Setup local file cache
+		File cacheDir = new File(System.getProperty("user.home") + File.separator + ".jxmapviewer2");
+		LocalResponseCache.installResponseCache(info.getBaseURL(), cacheDir, false);
+
+		// Setup JXMapViewer
+		final JXMapViewer mapViewer = new JXMapViewer();
+		mapViewer.setTileFactory(tileFactory);
+
+		
+
+		// Set the focus
+		mapViewer.setZoom(7);
+		mapViewer.setAddressLocation(new GeoPosition(47.53112 , 21.65992)); // debrecen
+
+		 //Create a track from the geo-positions
+		
+		List<GeoPosition> track = new ArrayList();
+		
+		java.util.Scanner scan;
+        	java.io.File file = new java.io.File ( "in" );
+
+        	long ref = 0;
+        	double lat = 0.0, lon = 0.0;
+        	try {
+
+           	 scan = new java.util.Scanner ( file );
+
+            		while ( scan.hasNext() ) {
+
+               		 
+               		 lat = scan.nextDouble();
+               		 lon = scan.nextDouble();
+			 track.add( new GeoPosition( lat, lon ));
+            		}
+
+       		 } catch ( Exception e ) {
+
+           		System.out.println("file not found");
+
+       		 }
+		RoutePainter routePainter = new RoutePainter(track);
+		
+		// Create waypoints from the geo-positions
+		Set<Waypoint> waypoints = new HashSet<Waypoint>(Arrays.asList(
+				new DefaultWaypoint(track.get(0)),
+				new DefaultWaypoint(track.get(track.size()-1))));
+
+		// Create a waypoint painter that takes all the waypoints
+		WaypointPainter<Waypoint> waypointPainter = new WaypointPainter<Waypoint>();
+		waypointPainter.setWaypoints(waypoints);	
+
+		// Add interactions
+		MouseInputListener mia = new PanMouseInputListener(mapViewer);
+		mapViewer.addMouseListener(mia);
+		mapViewer.addMouseMotionListener(mia);
+
+		mapViewer.addMouseListener(new CenterMapListener(mapViewer));
+		
+		mapViewer.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mapViewer));
+		
+		mapViewer.addKeyListener(new PanKeyListener(mapViewer));
+		
+		// Add a selection painter
+		SelectionAdapter sa = new SelectionAdapter(mapViewer); 
+		SelectionPainter sp = new SelectionPainter(sa); 
+		mapViewer.addMouseListener(sa); 
+		mapViewer.addMouseMotionListener(sa); 
+		//mapViewer.setOverlayPainter(sp);
+
+		// Create a compound painter that uses both the route-painter and the waypoint-painter
+		List<Painter<JXMapViewer>> painters = new ArrayList<Painter<JXMapViewer>>();
+		painters.add(routePainter);
+		painters.add(waypointPainter);
+
+		
+		
+		CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>(painters);
+		mapViewer.setOverlayPainter(painter);
+		
+		// Display the viewer in a JFrame
+		final JFrame frame = new JFrame();
+		frame.setLayout(new BorderLayout());
+		frame.add(new JLabel("Use left mouse button to pan, mouse wheel to zoom and right mouse to select"), BorderLayout.NORTH);
+		frame.add(mapViewer);
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		mapViewer.addPropertyChangeListener("zoom", new PropertyChangeListener()
+		{
+			@Override
+			public void propertyChange(PropertyChangeEvent evt)
+			{
+				updateWindowTitle(frame, mapViewer);
+			}
+		});
+		
+		mapViewer.addPropertyChangeListener("center", new PropertyChangeListener()
+		{
+			@Override
+			public void propertyChange(PropertyChangeEvent evt)
+			{
+				updateWindowTitle(frame, mapViewer);
+			}
+		});
+		
+		updateWindowTitle(frame, mapViewer);
+	}
+
+	protected static void updateWindowTitle(JFrame frame, JXMapViewer mapViewer)
+	{
+		double lat = mapViewer.getCenterPosition().getLatitude();
+		double lon = mapViewer.getCenterPosition().getLongitude();
+		int zoom = mapViewer.getZoom();
+		
+		frame.setTitle(String.format("JXMapviewer2 Example 3 (%.2f / %.2f) - Zoom: %d", lat, lon, zoom)); 
+	}
+	
+	
+}
